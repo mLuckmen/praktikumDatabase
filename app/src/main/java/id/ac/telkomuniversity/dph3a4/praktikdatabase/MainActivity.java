babfdb5bdb3fb84cde89e23929cd8b07ad3dec3d
mLuckmen <emluqman 101113@gmail.com>
2019-11-05 06:39:25 +0700

    datepicker

--- app/src/main/java/id/ac/telkomuniversity/dph3a4/praktikdatabase/MainActivity.java
+++ app/src/main/java/id/ac/telkomuniversity/dph3a4/praktikdatabase/MainActivity.java
@@ -2,17 +2,27 @@ package id.ac.telkomuniversity.dph3a4.praktikdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     EditText nipEdit, nameEdit;
     Button saveButton, showButton;
+    TextView dateText;
+    ImageButton btnDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
         nameEdit = findViewById(R.id.editNama);
         saveButton = findViewById(R.id.btnSimpan);
         showButton = findViewById(R.id.btnTampilkan);
+
+        dateText = findViewById(R.id.date_text);
+        btnDate = findViewById(R.id.btnDate);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +57,23 @@ public class MainActivity extends AppCompatActivity {
                 printDataToLog();
             }
         });
+    }
+
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date = dayOfMonth + "/" + (month+1) + "/" + year;
+        dateText.setText(date);
     }
 
     private void saveData() {
